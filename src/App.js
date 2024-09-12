4ca4ebb0a73427a44ca00ee1f21d9b550e10a0e1
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2024-09-12 02:21:12 -0300

    Change name value on button click without useState function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-// Prefer to put any site-wide images used in many different places inside the public folder.
-//import logo from './logo.svg'; // File moved from src to public folder
 import './App.css';
 
 function App() {
+
+  let name = "Sophia Loren";
+
+  const handleClick = () => {
+    name = "Elvis Presley";
+    console.log(name);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />*/} {/* 1st way to work with images */}
-        <img src='/logo.svg' className="App-logo" alt="logo" />   {/* 2nd way to work with images */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React & Firebase
-        </a>
-      </header>
+      <h1>Hello, my name is {name}</h1>
+      <button onClick={handleClick}>Change Name</button>      
     </div>
   );
 }
