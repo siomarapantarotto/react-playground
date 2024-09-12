bceac03e2b217fb5f9f2228c0c7eca8dcf2808e6
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2024-09-11 22:56:28 -0300

    Handle image locations and add documentation to CSS files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+// Prefer to put any site-wide images used in many different places inside the public folder.
+//import logo from './logo.svg'; // File moved from src to public folder
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />*/} {/* 1st way to work with images */}
+        <img src='/logo.svg' className="App-logo" alt="logo" />   {/* 2nd way to work with images */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React & Firebase
         </a>
       </header>
     </div>
