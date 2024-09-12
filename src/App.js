315c65c2deea4fffdffd5ab2d4b8fb677b98747a
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2024-09-12 03:30:52 -0300

    Change name value with useState method on the browser

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let name = "Sophia Loren";
+  // attribute name and method setName
+  const [name, setName] = useState("Sophia Loren"); 
 
   const handleClick = () => {
-    name = "Elvis Presley";
+    setName("Elvis Presley");
     console.log(name);
   }
   
@@ -20,8 +22,6 @@ function App() {
 export default App;
 
 /*
-  When you click the button, the name changes to “Elvis Presley” in the console, but not in the UI. 
-  This is because React doesn’t re-render the component when the state changes. 
-  To make the component re-render when the state changes, you need to use the useState hook. 
-  We’ll cover the useState hook in the next section.
+  When you click the button, the name changes to “Elvis Presley” 
+  in the browser, but the console.log(name) still logs “Sophia Loren”.
 */
