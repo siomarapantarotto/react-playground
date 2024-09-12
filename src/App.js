3a7ccfcb21f5312186ee1792595c2e2f303ed4a4
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2024-09-12 05:13:49 -0300

    Outputting list of events with id and index

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import { useState } from 'react';
 function App() {
 
   // attribute name and method setName
-  const [name, setName] = useState("Sophia Loren"); 
+  const [name, setName] = useState("Sophia Loren");
+  const [events, setEvents] = useState([
+    { title: "Sophia's birthday party", id: 1 },
+    { title: "Elvis's music concert", id: 2 },
+    { title: "John's wedding", id: 3 },
+    { title: "Sara's graduation", id: 4 }
+  ]);
 
   const handleClick = () => {
     setName("Elvis Presley");
@@ -14,7 +20,12 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, my name is {name}</h1>
-      <button onClick={handleClick}>Change Name</button>      
+      <button onClick={handleClick}>Change Name</button>
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+        </div>
+      ))}     
     </div>
   );
 }
