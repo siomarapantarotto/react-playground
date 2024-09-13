fb87dc29b59180935a9d7694f206c638f87b4b9a
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2024-09-13 02:29:04 -0300

    Comment out <React.StrictMode> just to avoid double rendering behavior when inspecting the code

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Commented out <React.StrictMode> just to avoid double rendering behavior while inspecting the code.
+// Note: It is a good practice to use <React.StrictMode> to help identify potential issues in development, 
+// such as side effects and deprecated APIs.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <App />
-  </React.StrictMode>
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
