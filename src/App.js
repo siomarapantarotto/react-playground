b3d34bc420683f7c2d9a7e3bec5dccdf02cff787
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2024-09-29 14:19:10 -0300

    Create single 'Title' component with subtitle applying basic styles and nesting it into App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import Title from './components/Title';
 
 function App() {
 
@@ -43,6 +44,8 @@ function App() {
   
   return (
     <div className="App">
+      <Title />
+      
       {showEvents && (
         <div>
           <button onClick={() => setShowEvents(false)}>Hide Events</button>
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+export default function Title() {
+
+    return (
+        <div>
+            <h1 className="title">World Fantastic Events</h1>
+            <br />
+            <h2 className="subtitle">All the latest events in the world</h2>
+        </div>
+    )
+}
