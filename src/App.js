49c0cad09832c00b9acce63b69ab191bc384dd08
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2024-10-17 22:21:29 -0300

    Create EventList component and pass events and handleClick as destructured props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Title from './components/Title';
 import Modal from './components/Modal';
+import EventList from './components/EventList';
 
 function App() {
 
@@ -49,13 +50,7 @@ function App() {
           <button onClick={() => setShowEvents(true)}>Show Events</button>
         </div>
       )}
-      {showEvents && events.map((event, index) => (
-        // long way to create a fragment on elements that have props inside
-        <React.Fragment key={event.id}>
-          <h2>{index} - {event.title}</h2>
-          <button onClick={() => {handleClick(event.id)}}>Delete Event</button>
-        </React.Fragment>
-      ))}    
+      {showEvents && <EventList events = {events} handleClick={handleClick}/>}    
 
       { showModal && <Modal handleClose={handleClose}>
         <h2>Terms and Conditions</h2>
